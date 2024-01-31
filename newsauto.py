089a6f9cf45de1b7f05dbc28d8ef6ea805908d55
RohanQ17 <axelb4467@gmail.com>
2024-01-31 15:07:27 +0530

    scraping headlines,subs,links and exporting it to a csv dataframe

--- newsauto.py
+++ newsauto.py
@@ -2,6 +2,8 @@ import selenium.webdriver.chromium.webdriver
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+from selenium.webdriver.chrome.options import Options
 
 news_site = "https://www.healthline.com/health-news"
 #path = "C:/Users/ROHAN/Downloads/chrome-win64/chrome-win64"
@@ -10,3 +12,22 @@ news_site = "https://www.healthline.com/health-news"
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get(news_site)
 
+barrels = driver.find_elements(by="xpath", value='//div[@class="css-8atqhb"]')
+
+headlines = []
+subs = []
+links = []
+for barrel in barrels:
+    headline = barrel.find_element(by="xpath", value='./a/h2').text
+    sub = barrel.find_element(by="xpath", value='./p/a').text
+    full_link = barrel.find_element(by="xpath", value='./a').get_attribute("href")
+    headlines.append(headline)
+    subs.append(sub)
+    links.append(full_link)
+
+
+compile_dict = {'headline': headlines, 'sub': subs, 'full_link': links}
+news_headings = pd.DataFrame(compile_dict)
+news_headings.to_csv('news.csv')
+
+driver.quit()
